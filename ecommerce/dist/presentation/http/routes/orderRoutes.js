6f9347ff883094bba22e06789cd32c71f1ef39e8
Zedan <zedanmohamedcp@gmail.com>
2026-08-13 07:03:38 +0300

    git ignore and remove cached

--- ecommerce/dist/presentation/http/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const OrderController_1 = require("../controllers/OrderController");
-const router = (0, express_1.Router)();
-router.post("/orders", OrderController_1.OrderController.create);
-exports.default = router;
--- ecommerce/dist/presentation/http/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ProductController_1 = require("../controllers/ProductController");
-const router = (0, express_1.Router)();
-router.post("/products", ProductController_1.ProductController.create);
-router.get("/products", ProductController_1.ProductController.getAll);
-exports.default = router;

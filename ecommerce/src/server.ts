0d97575da1827d709a055b01b6f87ad4463c912b
Zedan <zedanmohamedcp@gmail.com>
2026-08-14 18:24:09 +0300

    dockerize the backend

--- ecommerce/src/server.ts
+++ ecommerce/src/server.ts
@@ -12,6 +12,6 @@ app.use("/api", orderRoutes);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
